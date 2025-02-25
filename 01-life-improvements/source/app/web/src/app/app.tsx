72c505f807b77d8cde9a5257bd58b088ba6b6e62
Ryan Jandrick Obeles <51547772+rjbobeles@users.noreply.github.com>
2025-02-25 15:16:32 +0800

    Initial Commit

---
+++ 01-life-improvements/source/app/web/src/app/app.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes, Link, BrowserRouter } from 'react-router-dom';
+// Uncomment this line to use CSS modules
+// import styles from './app.module.scss';
+
+export function App() {
+  return (
+    <div>
+      <h1>
+        <span> Hello there, </span>
+        Welcome web 👋
+      </h1>
+    </div>
+  );
+}
+
+export default App;
