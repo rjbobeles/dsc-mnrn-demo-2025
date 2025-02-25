54a6b4593f5668f13b149a65f68d6694ae7a0372
Ryan Jandrick Obeles <51547772+rjbobeles@users.noreply.github.com>
2025-02-25 15:23:05 +0800

    Feat: Linting

--- 01-life-improvements/eslint.config.mjs
+++ 01-life-improvements/eslint.config.mjs
@@ -1,9 +1,20 @@
-import nx from "@nx/eslint-plugin";
+import nx from "@nx/eslint-plugin"
+import airbnbConfig from "eslint-config-airbnb"
+import prettierConfig from "eslint-config-prettier"
+import prettierPlugin from "eslint-plugin-prettier"
+
 
 export default [
     ...nx.configs["flat/base"],
     ...nx.configs["flat/typescript"],
     ...nx.configs["flat/javascript"],
+    ...airbnbConfig.flat,
+    {
+      plugins: {
+          prettier: prettierPlugin,
+      }
+    },
+    ...prettierConfig.flat,
     {
         ignores: [
             "**/dist"
@@ -47,7 +58,31 @@ export default [
             "**/*.cjs",
             "**/*.mjs"
         ],
-        // Override or add rules here
-        rules: {}
+        rules: {
+          "import/order": [
+            "error",
+            {
+              "groups": ["builtin", "external", "internal", "sibling", "index", "unknown"],
+              "newlines-between": "always",
+              "alphabetize": {
+                "order": "asc",
+                "caseInsensitive": true
+              }
+            }
+          ],
+          "radix": "off",
+          "dot-notation": "off",
+          "import/no-extraneous-dependencies": "off",
+          "import/no-named-default": "off",
+          "import/no-unresolved": "off",
+          "import/extensions": "off",
+          "import/prefer-default-export": "off",
+          "no-shadow": "off",
+          "no-underscore-dangle": "off",
+          "no-nested-ternary": "off",
+          "no-useless-constructor": "warn",
+          "class-methods-use-this": "warn",
+          "camelcase": "off"
+        }
     }
-];
+]
